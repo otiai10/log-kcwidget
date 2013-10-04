b970c569c0f4a03ecdcb527ca344dc8ebdd8fcee
otiai10 <otiai10@gmail.com>
2013-10-04 14:08:11 +0900

    connect to mgo (in controller :smiling_imp:)

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
   "github.com/robfig/revel"
+  "labix.org/v2/mgo"
 )
 
 type Application struct {
@@ -12,8 +13,35 @@ type Ocr struct {
   *revel.Controller
 }
 
+type Person struct {
+  Name  string
+  Phone string
+}
+
 func (c Application) Index() revel.Result {
-  return c.Render()
+
+  //----------------->>>
+  session, err := mgo.Dial("localhost")
+  if err != nil {
+          panic(err)
+  }
+  defer session.Close()
+  // Optional. Switch the session to a monotonic behavior.
+  session.SetMode(mgo.Monotonic, true)
+  con := session.DB("test").C("hoge")
+  //err = con.Insert(&Person{"Ale", "+55 53 8116 9639"},
+  //           &Person{"Cla", "+55 53 8402 8510"})
+  //if err != nil {
+  //        panic(err)
+  //}
+  people := []Person{}
+  err = con.Find(nil).All(&people)
+  if err != nil {
+          panic(err)
+  }
+  //----------------<<<
+
+  return c.Render(people)
 }
 
 func (c Ocr) Index() revel.Result {
