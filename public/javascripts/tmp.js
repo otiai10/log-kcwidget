35cfe04534536d6706b6438c39f4bb9aaead0297
otiai10 <otiai10@gmail.com>
2013-10-05 20:57:49 +0900

    change layout of Date

---
+++ public/javascripts/tmp.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('span.created-time').each(function(){
+    var s = $(this).html();
+    var d = new Date(parseInt(s));
+    $(this).html(d.toLocaleString());
+  });
+  $('.proxy').each(function(){
+    var uri = $(this).html();
+    var img = new Image();
+    img.src = uri;
+    $(this).after(img.outerHTML);
+    $(this).remove();
+  });
+});
